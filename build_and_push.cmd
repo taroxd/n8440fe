44f622e2a3f2b7a295e6f0eea2397c932a7209fc
taroxd <taroxd@outlook.com>
2019-03-25 17:55:54 +0800

    forgot git add ...

--- build_and_push.cmd
+++ build_and_push.cmd
@@ -4,6 +4,7 @@ git reset HEAD~
 git rebase master
 ruby epub\make_epub.rb
 latexmk -cd -f -lualatex -interaction=nonstopmode -synctex=1 n8440fe.tex
+git add .
 git commit -m "update"
 git push -f
 git checkout master
