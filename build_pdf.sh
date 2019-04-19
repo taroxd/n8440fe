d8fd7bc5d00285c777fc416aedc9616e2ff89552
Infko <recollectionforgot@gmail.com>
2019-04-19 19:12:34 +0800

    Update build_pdf.sh

--- build_pdf.sh
+++ build_pdf.sh
@@ -1,3 +1,16 @@
-tlmgr install luatexja
+sudo tlmgr install luatexja
+mkdir fonts
+cd fonts
+wget https://github.com/adobe-fonts/source-han-sans/raw/release/OTF/SourceHanSansJ.zip
+sudo apt-get install unzip
+unzip SourceHanSansJ.zip
+rm SourceHansSansJ.zip
+sudo mkdir -p /usr/share/fonts/custom
+sudo cp -r ./ /usr/share/fonts/custom
+sudo chmod -r 744 /usr/share/fonts/custom/*
+
+sudo mkfontscale 
+sudo mkfontdir
+
 cd /workdir
 latexmk -cd -f -lualatex -interaction=nonstopmode -synctex=1 n8440fe.tex
