7c7fc2d855a49d2121f8e34854e0cfaae2ebc74f
Infko <recollectionforgot@gmail.com>
2019-04-19 23:33:47 +0800

    Update build_pdf.sh

--- build_pdf.sh
+++ build_pdf.sh
@@ -4,11 +4,10 @@ cd fonts
 wget https://github.com/adobe-fonts/source-han-sans/raw/release/SubsetOTF/SourceHanSansJP.zip
 wget http://saigetsu.moe/Saigetsu/FZLTXIHK--GBK1-0.TTF
 unzip SourceHanSansJP.zip
-rm SourceHansSansJP.zip
 mkdir -p /usr/share/fonts/custom
 cp -r ./SourceHanSansJP/* /usr/share/fonts/custom
 cp FZLTXIHK--GBK1-0.TTF /usr/share/fonts/custom
-chmod -r 744 /usr/share/fonts/custom/*
+chmod 744 /usr/share/fonts/custom/*
 
 
 
@@ -16,6 +15,8 @@ mkfontscale
 mkfontdir
 
 fc-list
+
+echo "Finished Font import"
 
 cd /workdir
 latexmk -cd -f -lualatex -interaction=nonstopmode -synctex=1 n8440fe.tex
