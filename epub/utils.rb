bc4bf898bb65b56c526bf37cdf9f0ee63b993f16
taroxd <taroxd@outlook.com>
2019-04-21 09:37:38 +0800

    bug fix on epub generator

--- epub/utils.rb
+++ epub/utils.rb
@@ -16,15 +16,17 @@ def line_to_html(line, wrap: nil, attrib: nil)
   end
   line.gsub!(/\\jpfont\s+/, '')
   # \ruby{1}{2}
-  line.gsub!(/\\ruby\{(.+?)\}\{(.+?)\}/) do
-    rb = $1
-    rt = $2
+  line.gsub!(/\\ruby\{(.+)/) do
+    rb, pos = before_brace_with_index $1
+    rt, pos = before_brace_with_index $1, pos + 2
+    rest = line_to_html $1[(pos + 1)..]
     remove_format = lambda do |s|
-      s.delete!('|')
+      s.delete!('|{}')
     end
     remove_format.call rb
     remove_format.call rt
-    "<ruby>#{rb}<rt>#{rt}</rt></ruby>"
+
+    "<ruby>#{rb}<rt>#{rt}</rt></ruby>#{rest}"
   end
   return '<ul>' if line.include?('\\begin{itemize}')
   return '</ul>' if line.include?('\\end{itemize}')
@@ -108,24 +110,27 @@ def make_footnote(content, footnote_id)
   FOOTNOTE
 end
 
-def before_brace(string)
+def before_brace(string, start_pos = 0)
+  before_brace_with_index(string, start_pos).first
+end
+
+def before_brace_with_index(string, start_pos = 0)
   stack = 0
-  pos = 0
-  string.each_char do |c|
+  start_pos.upto(string.length - 1) do |pos|
+    c = string[pos]
+
     if c == '{'
       stack += 1
     elsif c == '}'
       stack -= 1
     end
 
-    pos += 1
-
     if stack < 0
-      return string[0, pos]
+      return string[start_pos...pos], pos
     end
   end
 
-  string
+  [string, string.length]
 end
 
 def least_2_digits(numbering)
