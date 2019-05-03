4493dd016a1b09440b7f2bdc78d5cfba71b2bd10
taroxd <taroxd@outlook.com>
2019-05-03 19:54:05 +0800

    minor bug fix on script

--- make_tex_recent_version.rb
+++ make_tex_recent_version.rb
@@ -7,7 +7,7 @@ end
 Dir.chdir __dir__
 desired_chapter_start = Integer ARGV[0]
 tex_src = File.read 'n8440fe.tex', mode: 'rb', encoding: 'utf-8'
-tex_src.sub!(/for i=1,(\d+),1/) do
+tex_src.gsub!(/for i=1,(\d+),1/) do
   "for i=#{desired_chapter_start},#{$1},1"
 end
 
