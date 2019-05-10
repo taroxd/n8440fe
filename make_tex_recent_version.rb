7475e2256ec39e425f99b7493c97ea22e5eb10cc
taroxd <taroxd@outlook.com>
2019-05-10 12:05:06 +0800

    update recent_version script

--- make_tex_recent_version.rb
+++ make_tex_recent_version.rb
@@ -7,8 +7,8 @@ end
 Dir.chdir __dir__
 desired_chapter_start = Integer ARGV[0]
 tex_src = File.read 'n8440fe.tex', mode: 'rb', encoding: 'utf-8'
-tex_src.gsub!(/for i=1,(\d+),1/) do
-  "for i=#{desired_chapter_start},#{$1},1"
+tex_src.gsub!(/\\addchaps\{(\d+)\}\{(\d+)\}/) do
+  "\\addchaps{#{[desired_chapter_start, $1.to_i].max}}{#{$2}}"
 end
 
 puts tex_src
