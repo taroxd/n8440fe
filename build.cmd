8ba138a3b5d8824799385e9272e17ee1edf2736d
taroxd <taroxd@outlook.com>
2019-05-17 07:10:50 +0800

    recent version from 71

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 @pushd %~dp0
 ruby epub\make_epub.rb
 latexmk -cd -f -lualatex -interaction=nonstopmode -synctex=1 n8440fe.tex
-ruby make_tex_recent_version.rb 67 > n8440fe_recent.tex
+ruby make_tex_recent_version.rb 71 > n8440fe_recent.tex
 latexmk -cd -f -lualatex -interaction=nonstopmode -synctex=1 n8440fe_recent.tex
 @popd
 @IF %0 == "%~0" pause
