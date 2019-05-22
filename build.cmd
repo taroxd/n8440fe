551722380737a87807e357fb97803ada60aa0cab
taroxd <taroxd@outlook.com>
2019-05-23 07:03:16 +0800

    update recent chapter to 78

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 @pushd %~dp0
 ruby epub\make_epub.rb
 latexmk -cd -f -lualatex -interaction=nonstopmode -synctex=1 n8440fe.tex
-ruby make_tex_recent_version.rb 74 > n8440fe_recent.tex
+ruby make_tex_recent_version.rb 78 > n8440fe_recent.tex
 latexmk -cd -f -lualatex -interaction=nonstopmode -synctex=1 n8440fe_recent.tex
 @popd
 @IF %0 == "%~0" pause
