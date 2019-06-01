da368fde0d8c2ec66f692fc6a22d278676d55cea
taroxd <taroxd@outlook.com>
2019-06-01 15:55:48 +0800

    add translator

--- epub/make_epub.rb
+++ epub/make_epub.rb
@@ -6,7 +6,8 @@ require_relative 'utils'
 title = '关于邻家的天使大人不知不觉把我惯成了废人这档子事'
 author = '佐伯さん'
 translators = ['taroxd', 'tongyuantongyu', '浪人',
-  'kae', '冰川镜华', '纱优纱优', '安诺尔', 'Konri', '葳蕤百媚生']
+  'kae', '冰川镜华', '纱优纱优', '安诺尔',
+  'Konri', '葳蕤百媚生', 'Muzz']
 reviewers = ['taroxd']
 
 Dir.chdir __dir__
