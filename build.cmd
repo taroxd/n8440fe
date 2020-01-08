b8c4bb7e43238a1c558c2a9df6ef753958aacdfb
taroxd <taroxd@outlook.com>
2020-01-08 18:57:09 +0800

    build: starting from 225

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 @pushd %~dp0
 ruby epub\make_epub.rb
 latexmk -cd -f -lualatex -interaction=nonstopmode n8440fe.tex
-ruby make_tex_recent_version.rb 220 > n8440fe_recent.tex
+ruby make_tex_recent_version.rb 225 > n8440fe_recent.tex
 latexmk -cd -f -lualatex -interaction=nonstopmode n8440fe_recent.tex
 @popd
 @IF %0 == "%~0" pause
